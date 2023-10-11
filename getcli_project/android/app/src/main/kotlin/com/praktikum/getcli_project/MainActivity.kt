0d2705d14414d9112c23e158f1560ca5f10b0e0f
xrenxf <kristinwulan113@gmail.com>
2023-10-11 21:53:11 +0700

    commit

---
+++ getcli_project/android/app/src/main/kotlin/com/praktikum/getcli_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.praktikum.getcli_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- getcli_project/lib/main.dart
+++ getcli_project/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import 'package:get/get.dart';
+
 import 'app/routes/app_pages.dart';
 
 void main() {
