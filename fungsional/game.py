fa99e2419864c4bfe968475b0744b8ae324a2d62
xrenxf <kristinwulan113@gmail.com>
2023-10-18 22:05:47 +0700

    commit

--- fungsional/game.py
+++ fungsional/game.py
@@ -1,72 +1,66 @@
 import random
 
+# Fungsi untuk membuat board matrix
 def create_board(width):
     return [['-' for _ in range(width)] for _ in range(width)]
 
-def print_board(board):
+# Fungsi untuk menampilkan board
+def display_board(board):
     for row in board:
         print(' '.join(row))
-        
-def generate_position(width):
-    return random.randint(0, width - 1), random.randint(0, width - 1)
 
-def move_piece(board, piece, position):
-    new_board = [row[:] for row in board]
-    x, y = position
-    new_board[x][y] = piece
-    return new_board
+# Fungsi untuk mengacak posisi awal bidak dan goals
+def generate_positions(width):
+    try:
+        start_position = (random.randint(0, width - 1), random.randint(0, width - 1))
+        goal_position = (random.randint(0, width - 1), random.randint(0, width - 1))
+        return start_position, goal_position
+    except Exception as e:
+        print(f"Terjadi kesalahan dalam mengenerate posisi: {e}")
+        return generate_positions(width)  # Coba lagi jika terjadi kesalahan
 
-def is_winner(position, goals):
-    return position in goals
+# Fungsi untuk menggerakkan bidak
+move_piece = lambda board, position, direction: (
+    (position[0] + direction[0], position[1] + direction[1])
+)
 
-def play_game():
+# Main game loop
+def main():
     width = int(input("Masukkan lebar board: "))
     board = create_board(width)
-    piece = 'A'
-    goals = []
+    start_position, goal_position = generate_positions(width)
 
-    for _ in range(3):
-        try:
-            goal = generate_position(width)
-            if goal not in goals:
-                goals.append(goal)
-        except Exception as e:
-            print("Gagal mengenerate goal: ", str(e))
+    board[start_position[0]][start_position[1]] = 'A'
+    board[goal_position[0]][goal_position[1]] = 'O'
 
-    print("Goals: ", goals)
-    
-    position = generate_position(width)
-    
-    while True:
-        print_board(board)
-        print("Posisi saat ini: ", position)
-        
-        if is_winner(position, goals):
-            print("Selamat, Anda menang!")
-            break
-        
-        move = input("Masukkan gerakan (w/a/s/d): ")
-        
+    display_board(board)
+
+    while start_position != goal_position:
+        move = input("Masukkan arah pergerakan (w/a/s/d): ").lower()
+
         if move == 'w':
-            new_position = (position[0] - 1, position[1])
+            direction = (-1, 0)
         elif move == 'a':
-            new_position = (position[0], position[1] - 1)
+            direction = (0, -1)
         elif move == 's':
-            new_position = (position[0] + 1, position[1])
+            direction = (1, 0)
         elif move == 'd':
-            new_position = (position[0], position[1] + 1)
+            direction = (0, 1)
         else:
-            print("Masukan tidak valid. Gunakan w/a/s/d.")
+            print("Arah pergerakan tidak valid. Gunakan w/a/s/d.")
             continue
-        
-        if 0 <= new_position[0] < width and 0 <= new_position[1] < width:
-            board = move_piece(board, '-', position)
-            position = new_position
-            board = move_piece(board, piece, position)
-        else:
-            print("Tidak bisa bergerak ke luar board!")
 
-    print_board(board)
+        new_position = move_piece(board, start_position, direction)
+
+        if 0 <= new_position[0] < width and 0 <= new_position[1] < width:
+            board[start_position[0]][start_position[1]] = ' '
+            start_position = new_position
+            board[start_position[0]][start_position[1]] = 'A'
+            display_board(board)
+        else:
+            print("Bidak tidak bisa keluar dari board.")
+
+    print("Selamat, Anda menang!")
 
 if __name__ == "__main__":
-    play_game()
+    main()
--- web/ToDoList.js
+++ web/ToDoList.js
@@ -1,18 +1,15 @@
-//Initial References
 const newTaskInput = document.querySelector("#new-task input");
 const tasksDiv = document.querySelector("#tasks");
 let deleteTasks, editTasks, tasks;
 let updateNote = "";
 let count;
 
-//Function on window load
 window.onload = () => {
   updateNote = "";
   count = Object.keys(localStorage).length;
   displayTasks();
 };
 
-//Function to Display The Tasks
 const displayTasks = () => {
   if (Object.keys(localStorage).length > 0) {
     tasksDiv.style.display = "inline-block";
@@ -20,23 +17,22 @@ const displayTasks = () => {
     tasksDiv.style.display = "none";
   }
 
-  //Clear the tasks
   tasksDiv.innerHTML = "";
 
-  //Fetch All The Keys in local storage
   let tasks = Object.keys(localStorage);
   tasks = tasks.sort();
 
   for (let key of tasks) {
     let classValue = "";
 
-    //Get all values
+    //semua values
     let value = localStorage.getItem(key);
     let taskInnerDiv = document.createElement("div");
     taskInnerDiv.classList.add("task");
     taskInnerDiv.setAttribute("id", key);
     taskInnerDiv.innerHTML = `<span id="taskname">${key.split("_")[1]}</span>`;
-    //localstorage would store boolean as string so we parse it to boolean back
+    
+    //edit & delete
     let editButton = document.createElement("button");
     editButton.classList.add("edit");
     editButton.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>`;
@@ -51,11 +47,11 @@ const displayTasks = () => {
     tasksDiv.appendChild(taskInnerDiv);
   }
 
-  //tasks completed
+  //completed
   tasks = document.querySelectorAll(".task");
   tasks.forEach((element, index) => {
     element.onclick = () => {
-      //local storage update
+      //update
       if (element.classList.contains("completed")) {
         updateStorage(element.id.split("_")[0], element.innerText, false);
       } else {
@@ -68,16 +64,11 @@ const displayTasks = () => {
   editTasks = document.getElementsByClassName("edit");
   Array.from(editTasks).forEach((element, index) => {
     element.addEventListener("click", (e) => {
-      //Stop propogation to outer elements (if removed when we click delete eventually rhw click will move to parent)
       e.stopPropagation();
-      //disable other edit buttons when one task is being edited
       disableButtons(true);
-      //update input value and remove div
       let parent = element.parentElement;
       newTaskInput.value = parent.querySelector("#taskname").innerText;
-      //set updateNote to the task that is being edited
       updateNote = parent.id;
-      //remove task
       parent.remove();
     });
   });
@@ -87,7 +78,6 @@ const displayTasks = () => {
   Array.from(deleteTasks).forEach((element, index) => {
     element.addEventListener("click", (e) => {
       e.stopPropagation();
-      //Delete from local storage and remove div
       let parent = element.parentElement;
       removeTask(parent.id);
       parent.remove();
@@ -116,9 +106,8 @@ const updateStorage = (index, taskValue, completed) => {
   displayTasks();
 };
 
-//Function To Add New Task
+//Add New Task
 document.querySelector("#push").addEventListener("click", () => {
-  //Enable the edit button
   disableButtons(false);
   if (newTaskInput.value.length == 0) {
     alert("Please Enter A Task");
