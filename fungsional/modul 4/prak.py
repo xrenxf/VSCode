07cbd9637b4b4dad45df611a759facfc2237bda4
xrenxf <kristinwulan113@gmail.com>
2023-11-21 14:44:14 +0700

    commit

--- fungsional/modul 4/prak.py
+++ fungsional/modul 4/prak.py
@@ -38,11 +38,11 @@ def line_equation_of(p1, p2):
     return f"y = {M:.2f}x + {C:.2f}"
 
 # TUGAS 1
-titik_A_ganjil = (3, 4)
+titik_A_ganjil = (2, 3)
 gradien_ganjil = 2
 
-titik_A_genap = (3, 4)
-titik_B_genap = (5, 6)
+titik_A_genap = (2, 3)
+titik_B_genap = (7, 7)
 
 # TUGAS 2
 translasi_aksi = translasi(2, -3)
@@ -57,19 +57,23 @@ titik_B_genap_transformed = perbesaran_aksi(*rotasi_aksi(*translasi_aksi(*titik_
 persamaan_garis_ganjil = line_equation_of(point(*titik_A_ganjil_transformed), point(*titik_A_ganjil))
 persamaan_garis_genap = line_equation_of(point(*titik_A_genap_transformed), point(*titik_B_genap_transformed))
 
-# Cetak hasil
-print("Koordinat hasil transformasi (NIM Ganjil):")
-cetak_koordinat(translasi_aksi, *titik_A_ganjil)
-cetak_koordinat(rotasi_aksi, *titik_A_ganjil_transformed)
-cetak_koordinat(perbesaran_aksi, *titik_A_ganjil_transformed)
+# print("Koordinat hasil transformasi (NIM Ganjil):")
+# cetak_koordinat(translasi_aksi, *titik_A_ganjil)
+# cetak_koordinat(rotasi_aksi, *titik_A_ganjil_transformed)
+# cetak_koordinat(perbesaran_aksi, *titik_A_ganjil_transformed)
+print("\nPersamaan garis sebelum transformasi (NIM Ganjil):")
+print(line_equation_of(point(*titik_A_ganjil), (titik_A_ganjil[0] + 1, titik_A_ganjil[1] + gradien_ganjil)))
 
 print("\nPersamaan garis hasil transformasi (NIM Ganjil):")
 print(persamaan_garis_ganjil)
 
-print("\nKoordinat hasil transformasi (NIM Genap):")
-cetak_koordinat(translasi_aksi, *titik_A_genap)
-cetak_koordinat(rotasi_aksi, *titik_A_genap_transformed)
-cetak_koordinat(perbesaran_aksi, *titik_A_genap_transformed)
+# print("\nKoordinat hasil transformasi (NIM Genap):")
+# cetak_koordinat(translasi_aksi, *titik_A_genap)
+# cetak_koordinat(rotasi_aksi, *titik_A_genap_transformed)
+# cetak_koordinat(perbesaran_aksi, *titik_A_genap_transformed)
+
+print("\nPersamaan garis sebelum transformasi (NIM Genap):")
+print(line_equation_of(point(*titik_A_genap), titik_B_genap))
 
 print("\nPersamaan garis hasil transformasi (NIM Genap):")
 print(persamaan_garis_genap)
